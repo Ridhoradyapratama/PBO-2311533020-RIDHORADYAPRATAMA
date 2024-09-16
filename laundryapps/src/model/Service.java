5c06585973aa17afdb686a3b1d41742ed0c76975
TUF Dash <TUF Dash@DHOVYYY>
2024-09-16 12:46:42 +0700

    pekan1

--- laundryapps/src/model/Service.java
+++ laundryapps/src/model/Service.java
@@ -1,38 +1,30 @@
 package model;
 
 public class Service {
-	String id, jenis, harga, status;
-
+	String id, jenis, status;
+	double harga;
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getJenis() {
 		return jenis;
 	}
-
 	public void setJenis(String jenis) {
 		this.jenis = jenis;
 	}
-
-	public String getHarga() {
-		return harga;
-	}
-
-	public void setHarga(String harga) {
-		this.harga = harga;
-	}
-
 	public String getStatus() {
 		return status;
 	}
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
+	public double getHarga() {
+		return harga;
+	}
+	public void setHarga(double harga) {
+		this.harga = harga;
+	}
 }
